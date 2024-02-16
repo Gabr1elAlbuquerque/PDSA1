727e13389e0383849cd151f25160d4743bff3576
Gabriel Mendes Albuquerque <albuquerque.g@aluno.ifsp.edu.br>
2024-02-15 22:02:45 -0300

    Criação de Menu

--- PDSA1/Program.cs
+++ PDSA1/Program.cs
@@ -2,9 +2,43 @@
 {
     internal class Program
     {
+        static void verificaTipoCarro() {
+            Console.WriteLine("Selecione o Tipo do Carro:");
+            Console.WriteLine("[1] Apenas Gasolina\n[2] Apenas Etanol\n[3] Flex");
+            
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            try
+            {
+            int opcaoMenu = 0;
+            Console.WriteLine("[1] Calcular Combustível\n[2] Editar Dados\n[3] Sair do Programa\n");
+            Console.Write("Digite sua escolha:");
+            opcaoMenu = int.Parse(Console.ReadLine());
+
+           
+                switch (opcaoMenu)
+                {
+                    case 1:
+                        verificaTipoCarro();
+                        break;
+                    case 2:
+                        Console.Write("B");
+                        break;
+                    case 3:
+                        Console.Write("C");
+                        break;
+                    default:
+                        Console.Write("Selecione uma opção válida!");
+                        break;
+
+                }
+            }
+            catch (Exception ex) {
+                Console.Write("Selecione uma opção válida!");
+            }
+       }
+
+
     }
 }
