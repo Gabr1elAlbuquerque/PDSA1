7bbff5ef47e3aab8ef3a01de74a074eb21be2720
Gabriel Mendes Albuqurque <albuquerque.g@aluno.ifsp.edu.br>
2024-02-22 22:01:46 -0300

    Criação do protóripo com as funções(inicioPrograma / validaOpcao / calculaProporcao / verificaTipoCarro/ calculaCombustivel)

--- PDSA1/Program.cs
+++ PDSA1/Program.cs
@@ -1,44 +1,127 @@
-﻿namespace PDSA1
+﻿using System.Runtime.CompilerServices;
+
+namespace PDSA1
 {
     internal class Program
     {
-        static void verificaTipoCarro() {
-            Console.WriteLine("Selecione o Tipo do Carro:");
-            Console.WriteLine("[1] Apenas Gasolina\n[2] Apenas Etanol\n[3] Flex");
-            
+        static void inicoPrograma(float proporcao)
+        {
+            int opcaoMenu = 0;
+            Console.WriteLine("[1] Calcular Combustível\n[2] Editar Dados\n[3] Sair do Programa\n");
+            opcaoMenu = validaOpcao();
+          
+
+            switch (opcaoMenu)
+            {
+                case 1:
+                    verificaTipoCarro(proporcao);
+                    Console.WriteLine();
+                    inicoPrograma(proporcao);
+                    break;
+                case 2:
+                    proporcao = calculaProporcao();
+                    Console.WriteLine();
+                    inicoPrograma(proporcao);
+                    break;
+                case 3:
+                    Console.WriteLine("\nSaindo do programa...");
+                    System.Threading.Thread.Sleep(1500);
+                    break;
+            }
+        }
+        static int validaOpcao()
+        {
+            bool isNumberResult = false;
+            int opcao = 0;
+            string opcaoAuxiliar = "";
+
+            while (true)
+            {
+                Console.Write("Digite sua escolha: ");
+                opcaoAuxiliar = Console.ReadLine();
+
+                isNumberResult = int.TryParse(opcaoAuxiliar, out opcao);
+                if (isNumberResult == false || opcao < 1 || opcao > 3)
+                {
+                    Console.WriteLine("Digite uma opção válida!");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return opcao;
+        }
+        static void verificaTipoCarro(float proporcao)
+        {
+            int tipoCarro = 0;
+            Console.WriteLine("\nSelecione o Tipo do Carro:");
+            Console.WriteLine("\n[1] Apenas Gasolina\n[2] Apenas Etanol\n[3] Flex\n");
+            tipoCarro = validaOpcao();
+            if (tipoCarro == 1)
+            {
+                Console.WriteLine("\nAbasteça com gasolina!");
+                Console.Write("\nDigite qualquer tecla para continuar!");
+                Console.ReadKey();
+                Console.WriteLine();
+
+            }
+            else if (tipoCarro == 2)
+            {
+                Console.WriteLine("\nAbasteça com etanol!");
+                Console.Write("\nDigite qualquer tecla para continuar!");
+                Console.ReadKey();
+                Console.WriteLine();
+            }
+            else
+            {
+                calculaCombustivel(proporcao);
+                Console.Write("\nDigite qualquer tecla para continuar!");
+                Console.ReadKey();
+                Console.WriteLine();
+
+            }
+        }
+
+        static float calculaProporcao() {
+
+            float proporcaoTemp = 0.0f, kmGasolina = 0.0f, kmEtanol = 0.0f;
+            Console.Write("\nDigite o consumo do carro utilizando gasolina Km/L: ");
+            kmGasolina = float.Parse(Console.ReadLine());
+            Console.Write("Digite o consumo do carro utilizando etanol Km/L: ");
+            kmEtanol = float.Parse(Console.ReadLine());
+
+            proporcaoTemp = ((kmEtanol * 100)  / kmGasolina) / 100;
+            return proporcaoTemp;
+        }
+
+        static void calculaCombustivel(float proporcao)
+        {
+            float precoGasolina = 0.00f, precoEtanol = 0.00f;
+            String custoBeneficio = "";
+
+            Console.Write("\nDigite o preço da gasolina R$: ");
+            precoGasolina = float.Parse(Console.ReadLine());
+            Console.Write("Digite o preço do etanol R$: ");
+            precoEtanol = float.Parse(Console.ReadLine());
+            if ((precoGasolina * proporcao) <= precoEtanol ) {
+                custoBeneficio = "Gasolina";
+            }
+            else
+            {
+                custoBeneficio = "Etanol";
+            }
+
+            Console.WriteLine($"\nAbasteça com {custoBeneficio}!");
+
         }
         static void Main(string[] args)
         {
-            try
-            {
-            int opcaoMenu = 0;
-            Console.WriteLine("[1] Calcular Combustível\n[2] Editar Dados\n[3] Sair do Programa\n");
-            Console.Write("Digite sua escolha:");
-            opcaoMenu = int.Parse(Console.ReadLine());
-
-           
-                switch (opcaoMenu)
-                {
-                    case 1:
-                        verificaTipoCarro();
-                        break;
-                    case 2:
-                        Console.Write("B");
-                        break;
-                    case 3:
-                        Console.Write("C");
-                        break;
-                    default:
-                        Console.Write("Selecione uma opção válida!");
-                        break;
-
-                }
-            }
-            catch (Exception ex) {
-                Console.Write("Selecione uma opção válida!");
-            }
-       }
-
-
+            float proporcao = 0.70f; ;
+            inicoPrograma(proporcao);
+            
+        }
     }
+
 }
+
